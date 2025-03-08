93fbe4f709d8686d134c8ecc6b01f55bfc30c612
Kotov <44296606+AIexandrKotov@users.noreply.github.com>
2025-03-08 21:32:11 +0300

    prod swagger

--- ArticleService/Program.cs
+++ ArticleService/Program.cs
@@ -25,7 +25,7 @@ namespace ArticleService
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
+            //if (app.Environment.IsDevelopment())
             {
                 app.UseSwagger();
                 app.UseSwaggerUI();
